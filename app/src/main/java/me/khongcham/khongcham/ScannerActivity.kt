44ea1a98b82c7bf44e2f826ee4c60ee6bd773604
Wallop Opasakhun <champbassist159@gmail.com>
2018-08-26 22:07:58 +0700

    Barcode Scanner added

--- app/src/main/java/me/khongcham/khongcham/ScannerActivity.kt
+++ app/src/main/java/me/khongcham/khongcham/ScannerActivity.kt
@@ -3,18 +3,60 @@ package me.khongcham.khongcham
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.ResultPoint
+import com.google.zxing.client.android.BeepManager
+import com.journeyapps.barcodescanner.BarcodeCallback
+import com.journeyapps.barcodescanner.BarcodeResult
+import com.journeyapps.barcodescanner.DecoratedBarcodeView
+import com.journeyapps.barcodescanner.DefaultDecoderFactory
 import kotlinx.android.synthetic.main.activity_scanner.*
 import me.khongcham.khongcham.Fragments.HomeFragment.Companion.scanRequestCode
+import java.util.*
 
-class ScannerActivity : AppCompatActivity(){
+
+class ScannerActivity : AppCompatActivity() {
 
     private var goodsId: Int? = null
     private var goodsName: String? = null
     private var goodsQuantity = 0
+    private var lastText: String? = null
+
+    private var barcodeView: DecoratedBarcodeView? = null
+    private var beepManager: BeepManager? = null
+
+    private val callback = object : BarcodeCallback {
+        override fun barcodeResult(result: BarcodeResult) {
+            if (result.text == null || result.text == lastText) return
+
+            lastText = result.text
+            barcodeView?.setStatusText(result.text)
+
+            beepManager?.playBeepSoundAndVibrate()
+
+            goodsLabel.text = result.text
+            goodsQuantity = 1
+            syncQuantity()
+
+        }
+
+        override fun possibleResultPoints(resultPoints: List<ResultPoint>) {}
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_scanner)
+
+        barcodeView = findViewById(R.id.barcode_scanner)
+        val formats = Arrays.asList(BarcodeFormat.CODABAR,
+                BarcodeFormat.UPC_A, BarcodeFormat.UPC_E, BarcodeFormat.UPC_EAN_EXTENSION,
+                BarcodeFormat.CODE_39, BarcodeFormat.CODE_128, BarcodeFormat.CODE_93,
+                BarcodeFormat.ITF, BarcodeFormat.EAN_13, BarcodeFormat.EAN_8)
+        barcodeView!!.barcodeView.decoderFactory = DefaultDecoderFactory(formats)
+        barcodeView!!.decodeContinuous(callback)
+        barcodeView?.barcodeView?.cameraSettings?.isAutoFocusEnabled = true
+
+        beepManager = BeepManager(this)
 
         button_cancel.setOnClickListener {
             finish()
@@ -38,6 +80,18 @@ class ScannerActivity : AppCompatActivity(){
         syncQuantity()
 
     }
+
+    override fun onResume() {
+        super.onResume()
+        barcodeView?.resume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        barcodeView?.pause()
+    }
+
 
     private fun syncQuantity() {
         card_stock_quantity.text = goodsQuantity.toString()
